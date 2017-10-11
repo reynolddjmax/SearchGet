ef83d439aac929474503d538e903727b8a625787
reynolddjmax <reynolddjmax@gmail.com>
2017-10-11 17:18:08 +0800

    Revert "B5改名"
    This reverts commit f591f9de615f02cd6119b326a296dbc5556344b8.

--- SearchGet/Form1.Designer.cs
+++ SearchGet/Form1.Designer.cs
@@ -243,7 +243,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 14;
-            this.button5.Text = "343434";
+            this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
