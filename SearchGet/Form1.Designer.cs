e346ab98458516294d4f76f3367b5d93feb7d6b1
reynolddjmax <reynolddjmax@gmail.com>
2017-10-11 16:10:05 +0800

    添加checkbox

--- SearchGet/Form1.Designer.cs
+++ SearchGet/Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -226,11 +227,22 @@
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(667, 488);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(78, 16);
+            this.checkBox6.TabIndex = 13;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(932, 513);
+            this.Controls.Add(this.checkBox6);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.checkBox5);
             this.Controls.Add(this.richTextBox2);
@@ -271,6 +283,7 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.CheckBox checkBox5;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox6;
     }
 }
 
